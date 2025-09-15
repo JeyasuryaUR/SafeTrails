9c9560449fc188884d7a1e4f093cc7a6e37d3924
Aalan sason singarayan A <131526989+aalan294@users.noreply.github.com>
2025-09-15 09:56:04 +0530

    Update app.ts
    cors update

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,14 +23,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({
-  origin: [
-    'http://localhost:8081',
-    /^http:\/\/localhost:\d+$/,
-    "exp://172.16.11.51:8081"
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
